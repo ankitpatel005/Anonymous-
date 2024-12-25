718004d17c5b35f99cd613f1250e30b59d4a4b10
ankitpatel005 <108638996+ankitpatel005@users.noreply.github.com>
2024-12-25 23:11:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ async def msg(_, message):
     btn = [[InlineKeyboardButton("🔍 Search Movies", url=f"https://t.me/{USER_NAME}")]]
     
     await message.reply_text(
-        f"<b>🏷 All movies Added .New movies everyday ⬇️⬇️\n\nhttps://t.me/apzmovie_series\nhttps://t.me/apzmovie_series\n\nSQUID GAME SEASON 2(MULTI - AUDIO HD) ⬇️\nJoin & type 👉 SQUID GAME S02 ⬇️\nhttps://t.me/+hWYNt0MArE8zZjh\nPUSHPA 2(MULTI - AUDIO HD) ⬇️https://t.me/+hWYNt0MArE8zZjhl\nSALAAR PART 2(MULTI - AUDIO HD) ⬇️\nhttps://t.me/+hWYNt0MArE8zZjhl</b>",
+        f"<b>🏷 All movies Added .New movies everyday ⬇️⬇️\n\nhttps://t.me/apzmovie_series\nhttps://t.me/apzmovie_series\n\nSQUID GAME SEASON 2(MULTI - AUDIO HD) ⬇️\nJoin & type 👉 SQUID GAME S02 ⬇️\nhttps://t.me/+hWYNt0MArE8zZjhl\n\nPUSHPA 2(MULTI - AUDIO HD) ⬇️https://t.me/+hWYNt0MArE8zZjhl\nSALAAR PART 2(MULTI - AUDIO HD) ⬇️\nhttps://t.me/+hWYNt0MArE8zZjhl</b>",
         reply_markup=InlineKeyboardMarkup(btn),
         disable_web_page_preview=True)
     return
